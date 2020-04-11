1ade5a7a0086aadbbc8424d57b248b2a52d2414c
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-11 14:57:25 -0500

    getting there

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,19 @@ function Homepage() {
   return(
     <div className="container">
       <div className="overlay">
+        <div className="content-box">
+          <div className="first-name">Rob</div>
+          <div className="last-name">Watkins</div>
+          <hr></hr>
+          <div className="web-dev">Web Developer</div>
+          <hr className="hr2"></hr>
+          <div className="tabs">
+            <div className="tab"></div>
+            <div className="tab"></div>
+            <div className="tab"></div>
+            <div className="tab"></div>
+          </div>
+        </div>
       </div>
     </div>
   )

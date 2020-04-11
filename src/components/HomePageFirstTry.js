f8566ed4deb69937003db04d4a56fc37e7528a86
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-11 18:03:11 -0500

    i just dont know

---
+++ src/components/HomePageFirstTry.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePageFirstTry = () => (
+  <div>
+    <h1>HomePageFirstTry, here.</h1>
+    <h4>Sup from an h4.</h4>
+  </div>
+)
+
+export default HomePageFirstTry;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,20 +4,36 @@ function Homepage() {
   return(
     <div className="container">
       <div className="overlay">
-        <div className="content-box">
+        <div className="nav-bar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        {/* <div className="tabs">
+          <div className="tab home">Home</div>
+          <div className="tab about">About</div>
+          <div className="tab portfolio">Portfolio</div>
+          <div className="tab contact">Contact</div>
+        </div> */}
+        <div className="title-box">
+          <hr className="top-bar"/>
           <div className="first-name">Rob</div>
           <div className="last-name">Watkins</div>
           <hr></hr>
           <div className="web-dev">Web Developer</div>
-          <div className="hr-tabs">
-            <hr className="hr2"></hr>
-            <div className="tabs">
-              <div className="tab home">Home</div>
-              <div className="tab about">About</div>
-              <div className="tab portfolio">Portfolio</div>
-              <div className="tab contact">Contact</div>
-            </div>
-          </div>
+          <hr className="hr2"></hr>
+        </div>
+        <div className="blog-box">
+          <h1>Recent Blog Posts</h1>
+          <h6>How Bloggy Should a Good Blog Be?</h6>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
+          <h6>How Bloggy Should a Good Blog Be?</h6>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
+          <h6>How Bloggy Should a Good Blog Be?</h6>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
         </div>
       </div>
     </div>

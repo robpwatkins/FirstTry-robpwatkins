861669d947df1406a6ddadaa5a299e2533813229
Rob W <robpwatkins@gmail.com>
2020-04-12 17:49:38 -0500

    wheeewwwwwwwwwwwwwwww

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 // import './reset.css';
-// import Homepage from './components/Homepage';
-import HomePageFirstTry from './components/HomePageFirstTry';
-import './FirstTry.css';
+import Homepage from './components/Homepage';
+// import HomePageFirstTry from './components/HomePageFirstTry';
+// import './FirstTry.css';
 // import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <HomePageFirstTry />
+      <Homepage />
     </div>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,40 +2,33 @@ import React from 'react';
 
 function Homepage() {
   return(
-    <div className="container">
-      <div className="overlay">
-        <div className="nav-bar">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        {/* <div className="tabs">
-          <div className="tab home">Home</div>
-          <div className="tab about">About</div>
-          <div className="tab portfolio">Portfolio</div>
-          <div className="tab contact">Contact</div>
-        </div> */}
-        <div className="title-box">
-          <hr className="top-bar"/>
-          <div className="first-name">Rob</div>
-          <div className="last-name">Watkins</div>
-          <hr></hr>
-          <div className="web-dev">Web Developer</div>
-          <hr className="hr2"></hr>
-        </div>
-        <div className="blog-box">
-          <h1>Recent Blog Posts</h1>
-          <h6>How Bloggy Should a Good Blog Be?</h6>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
-          <h6>How Bloggy Should a Good Blog Be?</h6>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
-          <h6>How Bloggy Should a Good Blog Be?</h6>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ipsum hic similique error nihil quasi architecto impedit unde accusantium modi quibusdam, molestiae eius commodi quam debitis vel quisquam. Et, consequuntur!</p>
+    <div className="main-container">
+      <div className="homepage-container">
+        <div className="container">
+          <div className="overlay">
+            <hr className="tippy-top" />
+            <div className="title-box">
+              <hr className="top-bar"/>
+              <div className="first-name">Rob</div>
+              <div className="last-name">Watkins</div>
+              <hr></hr>
+              <div className="web-dev">Web Developer</div>
+              {/* <hr className="hr2"></hr> */}
+            </div>
+            <footer>
+            <div className="nav-bar">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            </footer>
+          </div>
         </div>
       </div>
+      {/* <div className="about-container">About, here.</div> */}
     </div>
   )
 }

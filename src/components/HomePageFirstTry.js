a6ecc420e92503648c08926be02e7542bab8657a
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-12 12:11:51 -0500

    oh we got some traction baby

--- src/components/HomePageFirstTry.js
+++ src/components/HomePageFirstTry.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Bill from '../img/Bill-thumb.jpg';
 
 const HomePageFirstTry = () => (
   <div>
-    <h1>HomePageFirstTry, here.</h1>
-    <h4>Sup from an h4.</h4>
+    <header>
+      <hr/>
+      <nav>
+        <ul>
+          <li className="home">Home</li>
+          <li className="about">About</li>
+          <li className="portfolio">Portfolio</li>
+          <li className="contact">Contact</li>
+        </ul>
+      </nav>
+    </header>
+    <div className="main-container">
+      <div className="face-box">
+        <div className="inner-face">
+          <img src={Bill} alt=""/>
+        </div>
+      </div>
+      {/* <div className="name-box">
+        Rob Watkins
+      </div>
+      <hr className="hr-dev"/>
+      <div className="web-dev">
+        Web Developer
+      </div> */}
+    </div>
   </div>
 )
 
